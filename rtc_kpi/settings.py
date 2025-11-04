c38b0d12918a2a3a391ae7deec057edbb867ecb8
work4arun <149420950+work4arun@users.noreply.github.com>
2025-11-04 11:07:33 +0530

    host

--- rtc_kpi/settings.py
+++ rtc_kpi/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-dev-key-change-in-p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 INSTALLED_APPS = [
